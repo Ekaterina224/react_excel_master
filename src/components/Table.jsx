3ddd81c2c9c70cf6d8ea754b5c4d9d1de78ad523
Ekaterina Terenicheva <61419993+Ekaterina224@users.noreply.github.com>
2021-04-03 05:09:48 +0800

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,6 @@
-import React, { Component, ReactModal } from "react";
-import $ from "jquery";
+import React, { Component } from "react";
 import { forwardRef } from "react";
-import MaterialTable from "material-table";
+import MaterialTable from 'material-table';
 import AddBox from "@material-ui/icons/AddBox";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import Check from "@material-ui/icons/Check";
@@ -17,13 +16,9 @@ import Remove from "@material-ui/icons/Remove";
 import SaveAlt from "@material-ui/icons/SaveAlt";
 import Search from "@material-ui/icons/Search";
 import ViewColumn from "@material-ui/icons/ViewColumn";
-import Dialog from '@material-ui/core/Dialog';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import { Table } from "material-ui";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 class MaterialTableDemo extends Component {
   constructor(props) {
@@ -34,11 +29,13 @@ class MaterialTableDemo extends Component {
       modal_open: false,
       curIndex: -1,
       selectedItem: [],
+      teamSorting: false,
       collapseState: 'All Expand'
     }
     this.handleClose = this.handleClose.bind(this);
     this.handleOpen = this.handleOpen.bind(this);
     this.setOpen = this.setOpen.bind(this);
+    this.handleSorting = this.handleSorting.bind(this);
   }
 
   setOpen = (state) => {
@@ -49,23 +46,21 @@ class MaterialTableDemo extends Component {
     this.setOpen(false);
   };
 
+  handleSorting = () => {
+    if (this.state.teamSorting === true)
+      this.setState({ teamSorting: false });
+    else
+      this.setState({ teamSorting: true });
+  }
+
   handleOpen = (e) => {
     this.setOpen(true);
     var index = e.target.getAttribute("data-index");
-    if (typeof index == 'undefined' || index == null) {
+    if (typeof index === 'undefined' || index === null) {
       index = e.target.parentElement.getAttribute("data-index");
     }
     this.setState({ selectedItem: this.state.excelRows[index] });
   };
-
-  componentDidMount() {
-    $(".btn_collapse").click(function () {
-      if ($(this).html() == "All Expand")
-        $(this).html("All Collapse");
-      else
-        $(this).html("All Expand");
-    });
-  }
 
   render() {
     const tableIcons = {
@@ -95,136 +90,148 @@ class MaterialTableDemo extends Component {
       )),
       ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
     };
+
     var proList = [];
-    for (var item in this.state.excelRows) {
-      if (item == 0)
-        continue;
+    //this.productlist.shift();
+    for (var i = 1; i < this.state.excelRows.length - 1; i++) {
       proList.push({
-        No: item,
-        Phase: this.state.excelRows[item][0],
-        Service: this.state.excelRows[item][1],
-        Activity: this.state.excelRows[item][2],
-        Task: this.state.excelRows[item][3],
-        Action: ''
+        "no": i,
+        "id": -1,
+        "val1": this.state.excelRows[i][0],
+        "val2": this.state.excelRows[i][1],
+        "val3": this.state.excelRows[i][2],
+        "val4": this.state.excelRows[i][3],
+        "val5": this.state.excelRows[i][4],
+        "val6": this.state.excelRows[i][5],
+        "val7": this.state.excelRows[i][6],
+        "val8": this.state.excelRows[i][7],
       });
     }
 
-
-    return (
-      <div style={{ maxWidth: "90%", margin: "5vh auto" }} >
-        <button className="btn_collapse" onClick={() => {
-          var strBuf = '';
-          if (this.state.collapseState == "All Expand")
-            strBuf = 'All Collapse';
-          else
-            strBuf = 'All Expand';
-          var item;
-          for (item = 0; item < this.state.excelRows.length - 1; item++) {
-            this.tableRef.current.onToggleDetailPanel([item], rowData =>
-              <div>
-                <table className="col_table">
-                  <thead>
-                    <tr>
-                      <th className="col_th" style={{ width: '15%' }}>Description</th>
-                      <th className="col_th" style={{ width: '15%' }}>Team</th>
-                      <th className="col_th" style={{ width: '25%' }}>P&SC KPI</th>
-                      <th className="col_th" style={{ width: '25%' }}>Artefacts (Procedures/Forms etc. )</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className="col_td">{this.state.excelRows[rowData.No][4]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][5]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][6]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][7]}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>);
-          }
-        }}>{this.state.collapseState}</button>
-        <MaterialTable
-          tableRef={this.tableRef}
-          title="Product List"
-          icons={tableIcons}
-          expandedRows={true}
-          columns={[
-            { title: 'No', field: 'No' },
-            { title: 'Phase', field: 'Phase' },
-            { title: 'Service', field: 'Service' },
-            { title: 'Activity', field: 'Activity' },
-            { title: 'Task', field: 'Task', },
-            {
-              title: "Action",
-              render: (rowData) =>
-                rowData && (
-                  <Button data-index={rowData.No} color="secondary" className="btn_view" onClick={this.handleOpen} >View</Button>
-                )
+    function sortSubList(subList, index) {
+      var sub_key = 'val' + index;
+      var sort_list = {};
+      for (var item of subList) {
+        if (item[sub_key] in sort_list) {
+          sort_list[item[sub_key]].push(item);
+        }
+        else {
+          sort_list[item[sub_key]] = [item];
+        }
+      }
+      return (sort_list);
+    }
+    var data = []
+    var data_index = 1
+    var sortList = [];
+    if (this.state.teamSorting === true) {
+      var result = proList.reduce(function (r, a) {
+        r[a.val6] = r[a.val6] || [];
+        r[a.val6].push(a);
+        return r;
+      }, Object.create(null));
+      for (var key of Object.keys(result)) {
+        sortList = sortSubList(result[key], '1')
+        for (var item2 in sortList) {
+          var id2 = data_index;
+          data.push({ 'id': data_index++, val1: item2, val2: "", val3: "", val4: "", btn: "", val6: sortList[item2][0].val6, no: sortList[item2][0].no })
+          sortList[item2] = sortSubList(sortList[item2], '2');
+          for (var item3 in sortList[item2]) {
+            var id3 = data_index
+            data.push({ 'id': data_index++, val2: item3, val1: "", val3: "", val4: "", btn: "", parentId: id2, val6: sortList[item2][item3][0].val6, no: sortList[item2][item3][0].no })
+            sortList[item2][item3] = sortSubList(sortList[item2][item3], '3');
+            for (var item4 in sortList[item2][item3]) {
+              var id4 = data_index
+              data.push({ 'id': data_index++, val3: item4, val1: "", val2: "", val4: "", btn: "", parentId: id3, val6: sortList[item2][item3][item4][0].val6, no: sortList[item2][item3][item4][0].no })
+              sortList[item2][item3][item4] = sortSubList(sortList[item2][item3][item4], '4');
+              for (var item5 in sortList[item2][item3][item4]) {
+                data.push({ 'id': data_index++, val4: item5, val2: "", val3: "", val1: "", btn: "View", parentId: id4, val6: sortList[item2][item3][item4][item5][0].val6, no: sortList[item2][item3][item4][item5][0].no })
+              }
             }
-          ]}
-          data={proList}
-          detailPanel={rowData => {
-            return (
-              <div>
-                <table className="col_table">
+          }
+        }
+      }
+
+    }
+    else {
+      sortList = sortSubList(proList, '1')
+      for (var item2 in sortList) {
+        var id2 = data_index;
+        data.push({ 'id': data_index++, val1: item2, val2: "", val3: "", val4: "", btn: "", val6: sortList[item2][0].val6, no: sortList[item2][0].no })
+        sortList[item2] = sortSubList(sortList[item2], '2');
+        for (var item3 in sortList[item2]) {
+          var id3 = data_index
+          data.push({ 'id': data_index++, val2: item3, val1: "", val3: "", val4: "", btn: "", parentId: id2, val6: sortList[item2][item3][0].val6, no: sortList[item2][item3][0].no })
+          sortList[item2][item3] = sortSubList(sortList[item2][item3], '3');
+          for (var item4 in sortList[item2][item3]) {
+            var id4 = data_index
+            data.push({ 'id': data_index++, val3: item4, val1: "", val2: "", val4: "", btn: "", parentId: id3, val6: sortList[item2][item3][item4][0].val6, no: sortList[item2][item3][item4][0].no })
+            sortList[item2][item3][item4] = sortSubList(sortList[item2][item3][item4], '4');
+            for (var item5 in sortList[item2][item3][item4]) {
+              data.push({ 'id': data_index++, val4: item5, val2: "", val3: "", val1: "", btn: "View", parentId: id4, val6: sortList[item2][item3][item4][item5][0].val6, no: sortList[item2][item3][item4][item5][0].no })
+            }
+          }
+        }
+      }
+    }
+    console.log(sortList);
+    return (
+      <div style={{ maxWidth: "90%", margin: "5vh auto", }} >
+        <FormControlLabel
+          control={<Checkbox checked={this.state.teamSorting} onChange={this.handleSorting} name="team sorting" />}
+          label="Team Sorting"
+        />
+        <MaterialTable
+          detailPanel={[
+            {
+              icon: () => null,
+              disabled: true,
+              render: rowData => <div style={{ float: 'right', width: '60%' }}>
+                <table className="col_table" >
                   <thead>
                     <tr>
-                      <th className="col_th" style={{ width: '15%' }}>Description</th>
-                      <th className="col_th" style={{ width: '15%' }}>Team</th>
+                      <th className="col_th" style={{ width: '50%' }}>Description</th>
                       <th className="col_th" style={{ width: '25%' }}>P&SC KPI</th>
-                      <th className="col_th" style={{ width: '25%' }}>Artefacts (Procedures/Forms etc. )</th>
+                      <th className="col_th" style={{ width: '25%' }}>Artefacts</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="col_td">{this.state.excelRows[rowData.No][4]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][5]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][6]}</td>
-                      <td className="col_td">{this.state.excelRows[rowData.No][7]}</td>
+                      <td className="col_td">{this.state.excelRows[rowData.no][4]}</td>
+                      <td className="col_td">{this.state.excelRows[rowData.no][6]}</td>
+                      <td className="col_td">{this.state.excelRows[rowData.no][7]}</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-            )
+            }]
+          }
+          tableRef={this.tableRef}
+          title="Product List"
+          icons={tableIcons}
+          parentChildData={(row, rows) => rows.find(a => a.id === row.parentId)}
+          options={{
+            selection: false,
+
           }}
+
+          columns={[
+            { title: 'Phase', field: 'val1' },
+            { title: 'Service', field: 'val2', sorting: false },
+            { title: 'Activity', field: 'val3', sorting: false },
+            { title: 'Task', field: 'val4', sorting: false },
+            { title: 'Team', field: 'val6', sorting: false },
+            {
+              title: "Action",
+              render: (rowData) =>
+                rowData && (
+                  <Button data-index={rowData.no} color="secondary" className="btn_view" >{rowData.btn}</Button>
+                )
+            }
+          ]}
+          data={data}
+          onRowClick={(event, rowData, togglePanel) => rowData.btn === "View" && togglePanel()}
         />
-        <Dialog onClose={this.handleClose} aria-labelledby="customized-dialog-title" open={this.state.modal_open}>
-          <DialogTitle id="customized-dialog-title" onClose={this.handleClose} style={{ textAlign: 'center' }}>
-            More Information
-          </DialogTitle>
-          <DialogContent dividers >
-            <table className='md_table'>
-              <tbody>
-                <tr>
-                  <td className='mdb_td_w3'>Phase</td><td className='mdb_td_w7'>{this.state.selectedItem[0]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>Service</td><td className='mdb_td_w7'>{this.state.selectedItem[1]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>Activity</td><td className='mdb_td_w7'>{this.state.selectedItem[2]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>Task</td><td className='mdb_td_w7'>{this.state.selectedItem[3]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>Description</td><td className='mdb_td_w7'>{this.state.selectedItem[4]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>KPI</td><td className='mdb_td_w7' >{this.state.selectedItem[5]}</td>
-                </tr>
-                <tr>
-                  <td className='mdb_td_w3'>Artefact(s)</td><td className='mdb_td_w7'>{this.state.selectedItem[6]}</td>
-                </tr>
-              </tbody>
-            </table>
-          </DialogContent>
-          <DialogActions >
-            <Button autoFocus onClick={this.handleClose} color="secondary">
-              OK
-          </Button>
-          </DialogActions>
-        </Dialog>
       </div >
     )
   };
